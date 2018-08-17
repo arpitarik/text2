ae1916d3789bae73aaeff124e8c71cf5ea3ef558
arpitarik <42173385+arpitarik@users.noreply.github.com>
2018-08-17 11:17:52 +0530

    Add files via upload

---
+++ lab@3_q2.cpp
@@ -0,0 +1,23 @@
+// include the library
+#include<iostream>
+using namespace std;
+// write the function
+int main ()
+{
+//declaring the variables
+int a;
+float b;
+int c;
+//assigning the values
+a=5;
+b=3.2;
+c=2;
+
+//printing the results
+cout << "the sum is " << a+b << endl;
+cout << "the difference is" << a-b << endl;
+cout << "the product is" << a*b << endl;
+cout << "the quotient is" << a/b << endl;
+cout << "the modulus is" << a%c << endl;
+return 0;
+}
